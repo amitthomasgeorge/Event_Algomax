22bf31060807eb53b78ceb699d8e73b64ea347ac
Amit Thomas George <161476353+amitthomasgeorge@users.noreply.github.com>
2025-02-02 22:43:38 +0530

    limit web1.js

--- server/web1.js
+++ server/web1.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var con = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 30,
     host: 'bsat8p5xnva0yj1vchrm-mysql.services.clever-cloud.com',
     user: 'uianxt3quoyt8r0e',
     password: 'aRrSttlUE8ksq2fmwV3t',
