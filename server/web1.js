7e00816fdb2b95fddc930517ae9338ffdb98f62e
Amit Thomas George <161476353+amitthomasgeorge@users.noreply.github.com>
2025-02-02 20:04:44 +0530

    Update web1.js

--- server/web1.js
+++ server/web1.js
@@ -1,12 +1,28 @@
 var mysql = require("mysql")
-var con=mysql.createConnection({
+var dbconfig = {
     host: 'bsat8p5xnva0yj1vchrm-mysql.services.clever-cloud.com',
     user: 'uianxt3quoyt8r0e',
     password: 'aRrSttlUE8ksq2fmwV3t',
     database: 'bsat8p5xnva0yj1vchrm',
+}
+var con;
+function handle(){
+con=mysql.createConnection(dbconfig);
+con.connect(function(err){
+    if(err){
+        console.log('error when connect to db:',err);
+        setTimeout(handle,2000);
+    }
 });
-con.connect((error)=>{
-    if(error)  throw error;
+con.on('error',function(err){
+    console.log('db error',err);
+    if(err.code ==='PROTOCOL_CONNECTION_LOST'){
+        handle();
+    }
+    else{
+        throw err;
+    }
 });
-
+}
+handle();
 module.exports = con;
