fc2dc2400a857f10fd6bfef07df527c3fb710adc
Amit Thomas George <161476353+amitthomasgeorge@users.noreply.github.com>
2025-03-23 22:39:22 +0530

    Update web1.js

--- server/web1.js
+++ server/web1.js
@@ -1,15 +1,15 @@
-var mysql = require("mysql");
-
-var con = mysql.createPool({
-    connectionLimit: 50,
-    host: 'bsat8p5xnva0yj1vchrm-mysql.services.clever-cloud.com',
-    user: 'uianxt3quoyt8r0e',
-    password: 'aRrSttlUE8ksq2fmwV3t',
-    database: 'bsat8p5xnva0yj1vchrm',
-    connectTimeout: 28800000,
-    acquireTimeout: 28800000,
-    waitForConnections: true,
-    queueLimit: 0
+var mysql = require("mysql")
+require("dotenv").config();
+var con=mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+con.connect((error)=>{
+    if(error)  throw error;
 });
 
 module.exports = con;
+
+
